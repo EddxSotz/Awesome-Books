6b438f8e8f1bc26750bf69b53e9397348f8f2310
EddxSotz <96507617+EddxSotz@users.noreply.github.com>
2023-03-22 21:21:00 -0600

    created index.html and main.js

---
+++ main.js
@@ -0,0 +1,46 @@
+const form = document.getElementById('form');
+const bookList = document.getElementById('bookList');
+const btn = document.getElementById('btn');
+
+
+let booksList = JSON.parse(localStorage.getItem('books')) || [];
+
+
+ function addBook (title, author) {
+  const newBook = { title, author };
+  booksList.push(newBook);
+  localStorage.setItem("books", JSON.stringify(booksList));
+  displayBooks();
+ }
+
+ function removeBook(book) {
+  booksList = booksList.filter((a) => a !== book);
+  localStorage.setItem('books', JSON.stringify(booksList));
+  displayBooks();
+}
+
+ function displayBooks() {
+    bookList.innerHTML = '';
+    booksList.forEach((book) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${book.title} <br> by ${book.author} <br>`;
+    const removeBtn = document.createElement('button');
+    const hr = document.createElement('hr');
+    removeBtn.innerHTML = 'Remove';
+    removeBtn.addEventListener('click', () => removeBook(book));
+    li.appendChild(removeBtn);
+    bookList.appendChild(li);
+    bookList.appendChild(hr);
+  });
+};
+
+ displayBooks();
+ 
+ form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const bookTitle = document.getElementById('titleInput');
+  const bookAuthor = document.getElementById('authorInput');
+  addBook(bookTitle.value, bookAuthor.value);
+  titleInput.value = '';
+  authorInput.value = '';
+ });
